3e6946b121bb9fd06d634c894730befa96f91bfa
Nikhil Kaushik <nikhil.celtics@gmail.com>
2018-10-28 19:56:11 +0530

    chat works

--- public/app.js
+++ public/app.js
@@ -1,34 +1,35 @@
-window.onload = function() {
-    this.ws = new WebSocket('ws://' + window.location.host + '/ws');
-    this.ws.addEventListener('message', function(e) {
-        var msg = JSON.parse(e.data);
-        // do something with received message
-        newMsg(msg);
-    })
-    document.getElementById("msg-submit").addEventListener('submit', function() {
-        sendMessage()
-    });
-}
+this.ws = new WebSocket('ws://' + window.location.host + '/ws');
+this.ws.addEventListener('message', function(e) {
+    var msg = JSON.parse(e.data);
+    // do something with received message
+    newMsg(msg);
+})
+document.getElementById("msg-submit").addEventListener('click', function() {
+    sendMessage()
+});
+
+var email = null;
+var username = null;
 
 function sendMessage() {
-    var message = document.getElementById("msg").innerHTML; // todo parse
-    this.email = this.email || document.getElementById("email").value;
-    this.username = this.username || document.getElementById("username").value;
-    if (!email || !name) {
+    var message = document.getElementById("msg").value; // todo parse
+    email = document.getElementById("email").value;
+    username = document.getElementById("username").value;
+    if (!email || !username) {
         alert('Invalid email and/or username');
         return;
     }
-    this.ws.send(JSON.stringify(
+    this.ws.send(JSON.stringify({
         email,
         username,
         message
-    ));
+    }));
 }
 
 function newMsg(msg) {
     var msgCon = (document.getElementsByClassName("msgCon")[0]).cloneNode(true);
     msgCon.children[0].innerHTML = msg.email;
     msgCon.children[1].innerHTML = msg.username;
-    msgCon.children[2].innerHTML = msg.email;
+    msgCon.children[2].innerHTML = msg.message;
     document.getElementById("msgList").appendChild(msgCon);
 }
